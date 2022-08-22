543bd2763c554425371b934c605b84e822ec753b
GonzaloBernard <gonza.bernard.sf@gmail.com>
2022-08-22 15:33:47 -0300

    Limpieza

---
+++ resources/adminapp/js/cruds/Egresos/Index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-primary card-header-icon">
+                        <div class="card-icon"><i class="material-icons">assignment</i>
+                        </div>
+                        <h4 class="card-title">
+                            {{ $t('global.table') }}
+                            <strong>Egreso</strong>
+                        </h4>
+                    </div>
+
+                    <div class="card-body">
+                        <router-link class="btn btn-primary" :to="{ name: 'egresos.create' }">
+                            <i class="material-icons">
+                                add
+                            </i>
+                            {{ $t('global.add') }}
+                        </router-link>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h1>Tabla de Egresos</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  beforeDestroy() {
+    this.resetState()
+  },
+  created(){
+    console.log("asd")
+    this.fetchIndexData()
+  },
+  computed: {
+    ...mapGetters('EgresosIndex', ['data', 'total', 'loading'])
+  },
+  methods: {
+    ...mapActions('EgresosIndex', ['fetchIndexData', 'resetState'])
+  }
+}
+</script>
--- resources/adminapp/js/store/store.js
+++ resources/adminapp/js/store/store.js
@@ -3,32 +3,12 @@ import Vuex from 'vuex'
 
 import Alert from './modules/alert'
 import I18NStore from './modules/i18n'
-
 import PermissionsIndex from './cruds/Permissions'
 import PermissionsSingle from './cruds/Permissions/single'
 import RolesIndex from './cruds/Roles'
 import RolesSingle from './cruds/Roles/single'
 import UsersIndex from './cruds/Users'
 import UsersSingle from './cruds/Users/single'
-import ProductCategoriesIndex from './cruds/ProductCategories'
-import ProductCategoriesSingle from './cruds/ProductCategories/single'
-import ProductTagsIndex from './cruds/ProductTags'
-import ProductTagsSingle from './cruds/ProductTags/single'
-import ProductsIndex from './cruds/Products'
-import ProductsSingle from './cruds/Products/single'
-import ContactCompaniesIndex from './cruds/ContactCompanies'
-import ContactCompaniesSingle from './cruds/ContactCompanies/single'
-import ContactContactsIndex from './cruds/ContactContacts'
-import ContactContactsSingle from './cruds/ContactContacts/single'
-import ExpenseCategoriesIndex from './cruds/ExpenseCategories'
-import ExpenseCategoriesSingle from './cruds/ExpenseCategories/single'
-import IncomeCategoriesIndex from './cruds/IncomeCategories'
-import IncomeCategoriesSingle from './cruds/IncomeCategories/single'
-import ExpensesIndex from './cruds/Expenses'
-import ExpensesSingle from './cruds/Expenses/single'
-import IncomesIndex from './cruds/Incomes'
-import IncomesSingle from './cruds/Incomes/single'
-import ExpenseReports from './cruds/ExpenseReports'
 import EventosIndex from './cruds/Eventos'
 import EventoSingle from './cruds/Eventos/single'
 
@@ -46,25 +26,7 @@ export default new Vuex.Store({
     RolesSingle,
     UsersIndex,
     UsersSingle,
-    ProductCategoriesIndex,
-    ProductCategoriesSingle,
-    ProductTagsIndex,
-    ProductTagsSingle,
-    ProductsIndex,
-    ProductsSingle,
-    ContactCompaniesIndex,
-    ContactCompaniesSingle,
-    ContactContactsIndex,
-    ContactContactsSingle,
-    ExpenseCategoriesIndex,
-    ExpenseCategoriesSingle,
-    IncomeCategoriesIndex,
-    IncomeCategoriesSingle,
-    ExpensesIndex,
-    ExpensesSingle,
-    IncomesIndex,
-    IncomesSingle,
-    ExpenseReports,
+    // ENTIDADES DEL MODELO
     EventosIndex,
     EventoSingle
   },
