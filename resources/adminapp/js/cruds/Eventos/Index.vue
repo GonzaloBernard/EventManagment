ce924fe57415a8bcac04117b6f93b7a30834cd2f
GonzaloBernard <gonza.bernard.sf@gmail.com>
2022-08-24 23:43:19 -0300

    tabla de eventos

--- resources/adminapp/js/cruds/Eventos/Index.vue
+++ resources/adminapp/js/cruds/Eventos/Index.vue
@@ -13,17 +13,54 @@
                     </div>
 
                     <div class="card-body">
-                        <router-link class="btn btn-primary" :to="{ name: 'eventos.create' }">
-                            <i class="material-icons">
-                                add
-                            </i>
-                            {{ $t('global.add') }}
-                        </router-link>
+                       <v-btn
+                            color="primary"
+                            outlined
+                            class="mr-4"
+                            @click.stop="createEvent"
+                            >Nuevo</v-btn
+                        >
+                        <CreateEditModal
+                            :dialog="dialog"
+                            :scenario="scenario"
+                            :id="selectedEvent.id"
+                            @dialogClose="dialog = false"
+                            @eventAdded="fetchIndexData"
+                            />
                     </div>
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-12">
-                                <h1>Tabla de Eventos</h1>
+                                <template>
+                                    <v-card>
+                                        <v-card-title>
+                                            <v-text-field
+                                                v-model="search"
+                                                append-icon="mdi-magnify"
+                                                label="Search"
+                                                single-line
+                                                hide-details
+                                            ></v-text-field>
+                                        </v-card-title>
+                                        <v-data-table
+                                            :headers="headers"
+                                            :items="data"
+                                            :search="search"
+                                        >
+                                        <template v-slot:[`item.lugar`]="{ item }">
+                                        <v-chip>
+                                            {{item.lugar?.descripcion }}
+                                        </v-chip>
+                                        </template>
+
+                                        <template v-slot:[`item.acciones`]="{ item }">
+                                        <v-btn @click="destroyDataAction(item.id)">
+                                            Borrar
+                                        </v-btn>
+                                        </template>
+                                        </v-data-table>
+                                    </v-card>
+                                </template>
                             </div>
                         </div>
                     </div>
@@ -35,14 +72,29 @@
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
-
+import CreateEditModal from "./CreateEditModal.vue";
 export default {
-  components: {
-  },
-  data() {
-    return {
-    }
-  },
+    components: {
+        CreateEditModal,
+    },
+    data () {
+      return {
+        search: '',
+        headers: [
+          { text: 'Nombre', value: 'nombre'},
+          { text: 'Descripcion', value: 'descripcion' },
+          { text: 'Cliente', value: 'cliente' },
+          { text: 'Color', value: 'color' },
+          { text: 'Fecha', value: 'fecha' },
+          { text: 'Duración', value: 'duracion' },
+          { text: 'Lugar', value: 'lugar' },
+          { text: 'Acciones', value: 'acciones' },
+        ],
+        dialog: false,
+        scenario: '',
+        selectedEvent: {},
+      }
+    },
   beforeDestroy() {
     this.resetState()
   },
@@ -54,7 +106,29 @@ export default {
     ...mapGetters('EventosIndex', ['data', 'total', 'loading'])
   },
   methods: {
-    ...mapActions('EventosIndex', ['fetchIndexData', 'resetState'])
+    ...mapActions('EventosIndex', ['fetchIndexData', 'resetState', 'destroyData']),
+    destroyDataAction(id) {
+        this.$swal({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Delete',
+            confirmButtonColor: '#dd4b39',
+            focusCancel: true,
+            reverseButtons: true
+            }).then(
+                result => {
+                if (result.value) {
+                    this.destroyData(id)
+                }})
+    },
+    createEvent () {
+        //this.resetState()
+        this.scenario = 'create'
+        this.dialog = true
+    },
+
   }
 }
 </script>
